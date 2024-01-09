aa87c608e0e966c5e771cd44fe51e4ae0a7657af
Beata Konya <beatakonya@gmail.com>
2024-01-09 21:16:17 +0000

    changed code as worried i misunderstand the task

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,3 +3,47 @@ var today = dayjs().format("dddd, MMMM D YYYY ");
 var now = dayjs().format("H A");
 
 $("#currentDay").text(today);
+
+// var planWorkday = [
+//     {
+//         time: "9 AM",
+//         event: "" 
+//     },
+//     {
+//         time: "10 AM",
+//         event: ""
+//     }, 
+//     {
+//         time: "11 AM",
+//         event: ""
+//     },
+//     {
+//         time: "12 AM",
+//         event: ""
+//     },
+//     {
+//         time: "1 PM",
+//         event: ""
+//     },
+//     {
+//         time: "2 PM",
+//         event: ""
+//     },
+//     {
+//         time: "3 PM",
+//         event: ""
+//     },
+//     {
+//         time: "4 PM",
+//         event: ""
+//     },
+//     {
+//         time: "5 PM",
+//         event: ""
+//     },
+// ];
+
+// var workEvents = JSON.parse(localStorage.getItem('workDay'));
+// if (workEvents) {
+//     planWorkday = workEvents
+// };
